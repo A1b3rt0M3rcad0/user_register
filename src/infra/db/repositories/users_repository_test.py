e5d19e8c34137baea466ad93cc4dd8c79e77fb93
alberto <alberto.zimba2013@gmail.com>
2025-02-19 15:36:16 -0300

    test: Creating test_update_user_repository

--- src/infra/db/repositories/users_repository_test.py
+++ src/infra/db/repositories/users_repository_test.py
@@ -54,3 +54,22 @@ def test_delete_user_repository() -> None:
     user_repository.delete(mocked_username)
     users = select_user(engine)
     assert not users
+
+@set_up
+def test_update_user_repository() -> None:
+
+    mocked_username = 'username'
+    mocked_username_2 = 'username_2'
+    mocked_password = 'password'
+    mocked_created_at = datetime.now(timezone.utc)
+
+    user_repository = UsersRepository(TestStringConnection)
+    engine = user_repository.get_db_connection_handler().get_engine()
+    insert_user(mocked_username, mocked_password, mocked_created_at, engine)
+
+    user_repository.update(mocked_username, {'username': mocked_username_2})
+
+    users = select_user(engine)
+    user = users[0]
+
+    assert user.username == mocked_username_2
