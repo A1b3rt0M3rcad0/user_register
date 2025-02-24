9777743fccf18c49777cc994485611b57779e2d2
alberto <alberto.zimba2013@gmail.com>
2025-02-24 14:57:08 -0300

    feat: Implementing request validators

---
+++ src/validators/request_validators/change_username_validator_request.py
@@ -0,0 +1,14 @@
+from src.errors.types.http_unprocessable_entity_error import HttpUnprocessableEntityError
+from cerberus import Validator
+
+def change_username_validator(request:any) -> None:
+    
+    query_validator = Validator({
+        "username": {"type": "string", "required": True, "empty": False},
+        "new_username": {"type": "string", "required": True, "empty": False}
+    })
+
+    response = query_validator.validate(request.json)
+
+    if response is False:
+        raise HttpUnprocessableEntityError(query_validator.errors)
---
+++ src/validators/request_validators/select_user_validator_request.py
@@ -0,0 +1,13 @@
+from src.errors.types.http_unprocessable_entity_error import HttpUnprocessableEntityError
+from cerberus import Validator
+
+def select_user_validator(request:any) -> None:
+    
+    query_validator = Validator({
+        "username": {"type": "string", "required": True, "empty": False}
+    })
+
+    response = query_validator.validate(request.args)
+
+    if response is False:
+        raise HttpUnprocessableEntityError(query_validator.errors)
