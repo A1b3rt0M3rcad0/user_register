dc5d064f65f26e506bfce2d5d11c865998fbcb85
alberto <alberto.zimba2013@gmail.com>
2025-02-24 22:47:30 -0300

    refact: Refactoring return of the AuthenticateToken Interface

--- src/domain/use_cases/i_authenticate_token.py
+++ src/domain/use_cases/i_authenticate_token.py
@@ -3,4 +3,4 @@ from abc import ABC, abstractmethod
 class IAuthenticateToken(ABC):
 
     @abstractmethod
-    def authenticate(self, token:str) -> bool:pass
+    def authenticate(self, token:str) -> None:pass
